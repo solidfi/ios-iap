eee9b4ac36dfc269bffad4e3c835dbe634d5aa26
Rohan Panchal <67891512+rohan-wise@users.noreply.github.com>
2022-06-03 15:59:17 +0530

    added required files

---
+++ CardProvisioningVC.swift
@@ -0,0 +1,90 @@
+//  Solid
+//
+//  Created by Solid iOS Team on 03/06/22.
+
+import Foundation
+import UIKit
+import PassKit
+
+class CardProvisioningVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Check eligibility whether device supports In-app provisioning(add to wallet) or not..
+        self.checkEligibility()
+    }
+}
+
+// MARK: - Passkit methods
+extension CardInfoVC: PKAddPaymentPassViewControllerDelegate {
+    func checkEligibility() {
+        let eligibility = PKAddPaymentPassViewController.canAddPaymentPass()
+        let passLibrary = PKPassLibrary.init()
+        
+        if eligibility {
+            //once its eligible, add button to add card to wallet..
+            let addPassButton = PKAddPassButton()
+            addPassButton.addPassButtonStyle = .black
+            addPassButton.addTarget(self, action: #selector(startApplePayProvisioning), for: .touchUpInside)
+            self.view.addSubview(addPassButton)
+            self.view.bringSubviewToFront(addPassButton)
+            
+            addPassButton.translatesAutoresizingMaskIntoConstraints = false
+            addPassButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 16).isActive = true
+            addPassButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -25).isActive = true
+            addPassButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -16).isActive = true
+        }
+    }
+    
+    @objc func startApplePayProvisioning() {
+        let request = PKAddPaymentPassRequestConfiguration(encryptionScheme: .ECC_V2)
+        request?.cardholderName = "xxxxx"
+        request?.primaryAccountSuffix = "xxxxx" //last 4 digits of card
+        request?.localizedDescription =  "xxxxx" // description
+        request?.primaryAccountIdentifier = "xxxxx" //card id..... it filters the device and attached devices that already have this card provisioned.(Maybe cardId);
+        request?.paymentNetwork = PKPaymentNetwork.visa
+        let vc = PKAddPaymentPassViewController(requestConfiguration: request!, delegate: self)
+        self.present(vc!, animated: true, completion: nil)
+    }
+    
+    func addPaymentPassViewController(_ controller: PKAddPaymentPassViewController, generateRequestWithCertificateChain certificates: [Data], nonce: Data, nonceSignature: Data, completionHandler handler: @escaping (PKAddPaymentPassRequest) -> Void) {
+        
+        //First you should get the certificates, nonce and nonceSignature
+        //and convert them to Base 64 string.....
+        let certificateLeaf = certificates[0].base64EncodedString()
+        let nonceString = nonce.base64EncodedString()
+        let nonceSignatureVal = nonceSignature.base64EncodedString()
+
+        //After that, send it to backend...
+        var postBody = CardWalletRequestBody()
+        var aPay = Applepay()
+        aPay.nonce = nonceString
+        aPay.nonceSignature = nonceSignatureVal
+        aPay.deviceCert = certificateLeaf
+        postBody.wallet = "applePay"
+        postBody.applePay = aPay
+
+        APIcalls.enrollCard(cardId: cId, walletData: postBody) { response, errorMessage in
+            //you should get values in base64 string from the backend..
+            let payloadData = respCardModel.applePay?.encryptedPassData
+            let activationcode = respCardModel.applePay?.activationData
+            let ephemeralkey = respCardModel.applePay?.ephemeralPublicKey
+
+            //once you get the response from backend, convert it to base64 data
+            let encryptedPassData =  Data(base64Encoded: payloadData ?? "", options: [])
+            let ephemeralPublicKey  = Data(base64Encoded: ephemeralkey ?? "", options: [])
+            let activationData = Data(base64Encoded: activationcode ?? "", options: [])
+
+            //then create an object using PKAddPaymentPassRequest and send by completionHandler.
+            let paymentPassRequest = PKAddPaymentPassRequest.init()
+            paymentPassRequest.activationData = activationData
+            paymentPassRequest.encryptedPassData = encryptedPassData
+            paymentPassRequest.ephemeralPublicKey = ephemeralPublicKey
+            handler(paymentPassRequest)
+        }
+    }
+    
+    func addPaymentPassViewController(_ controller: PKAddPaymentPassViewController, didFinishAdding pass: PKPaymentPass?, error: Error?) {
+        UIApplication.shared.keyWindow?.rootViewController?.dismiss(animated: true, completion: nil)
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # ios-iap
 iOS In App Push Provisioning Sample
+
+## Table of contents
+- [Overview](#overview)
+- [Getting started](#getting-started)
+- [Prerequisite](#prerequisite)
+- [Integration Reference](#integration-reference)
+- [API Reference](#api-reference)
+
+## Overview
+Apple In App Provisioning is the way to provision your card to apple wallet.
+
+## Getting started
+Configure VGSShow Sample :
+- Clone repository
+```groovy
+git clone git@github.com:solidfi/ios-iap.git
+OR
+git clone https://github.com/solidfi/ios-iap.git
+```
+- Open CardProvisioningVC.swift file
+
+## Prerequisite
+- Request for the Apple Pay Entitlements to : applepayentitlements@apple.com
+- Once its granted, to enable In-App Provisioning, visit the Identifiers section for Certificates, Identifiers, and Profiles in Developer account. Edit any required App IDs and enable the In-App Provisioning capability in the new Additional Capabilities tab. Once enabled, the entitlement will automatically be included in newly generated provisioning profiles.
+- Add Wallet capability in Xcode's Signing & Capabilities  
+- After adding capability, add com.apple.developer.payment-pass-provisioning key and set it to true in Wallet entitlements
+
+NOTE: You cant test In App Provisioning only in Test flight or App store build. 
+
+## Integration Reference
+- Integration reference : https://usermanual.wiki/Document/InApp20Provisioning20Getting20Started20Guide20V123.884317268/view
+
+## API Reference
+- Solid API Integration : https://documenter.getpostman.com/view/13543869/TWDfEDwX#ce8c0e57-0dcf-45ea-87d8-6f03a302e027
